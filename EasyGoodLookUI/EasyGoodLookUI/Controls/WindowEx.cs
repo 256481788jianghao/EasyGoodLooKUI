6ebd2a3075e42a6c9c42898f13e2ccbfafedfb71
jianghao <2564811788@163.com>
2020-06-29 18:20:48 +0800

    临时修改 temp modify

---
+++ EasyGoodLookUI/EasyGoodLookUI/Controls/WindowEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace EasyGoodLookUI
+{
+    public class WindowEx:Window
+    {
+        static WindowEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WindowEx), new FrameworkPropertyMetadata(typeof(WindowEx)));
+        }
+
+        //#region Background
+        //public Brush Background
+        //{
+        //    get { return (Brush)GetValue(BackgroundProperty); }
+        //    set { SetValue(BackgroundProperty, value); }
+        //}
+        //public static readonly DependencyProperty BackgroundProperty =
+        //    DependencyProperty.Register("Background", typeof(Brush), typeof(WindowEx));
+        //#endregion
+    }
+}
--- EasyGoodLookUI/UIBrowser/MainWindow.xaml.cs
+++ EasyGoodLookUI/UIBrowser/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyGoodLookUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace UIBrowser
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : WindowEx
     {
         public MainWindow()
         {
