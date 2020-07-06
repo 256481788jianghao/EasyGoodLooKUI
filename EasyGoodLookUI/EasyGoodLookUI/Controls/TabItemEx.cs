78257ee03d371bfcf2aea5ddb157121731bb4dd6
jianghao <2564811788@163.com>
2020-07-06 13:23:22 +0800

    tabitem icon

--- EasyGoodLookUI/EasyGoodLookUI/Controls/TabItemEx.cs
+++ EasyGoodLookUI/EasyGoodLookUI/Controls/TabItemEx.cs
@@ -14,5 +14,31 @@ namespace EasyGoodLookUI
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TabItemEx), new FrameworkPropertyMetadata(typeof(TabItemEx)));
         }
+
+        #region Property
+        /// <summary>
+        /// Icon的内容
+        /// </summary>
+        public object IconContent
+        {
+            get { return GetValue(IconContentProperty); }
+            set { SetValue(IconContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconContentProperty =
+            DependencyProperty.Register("IconContent", typeof(object), typeof(TabItemEx));
+
+        /// <summary>
+        /// 是否显示Icon
+        /// </summary>
+        public bool ShowIcon
+        {
+            get { return (bool)GetValue(ShowIconProperty); }
+            set { SetValue(ShowIconProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowIconProperty =
+            DependencyProperty.Register("ShowIcon", typeof(bool), typeof(TabItemEx));
+        #endregion
     }
 }
