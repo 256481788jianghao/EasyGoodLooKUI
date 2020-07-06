84b54dabdb03da41873ab51e0d01c35ea6eea463
jianghao <2564811788@163.com>
2020-07-06 11:14:50 +0800

    改变TabControl的样式

---
+++ EasyGoodLookUI/EasyGoodLookUI/Controls/TabControlEx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EasyGoodLookUI
+{
+    public class TabControlEx:TabControl
+    {
+        static TabControlEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TabControlEx), new FrameworkPropertyMetadata(typeof(TabControlEx)));
+        }
+    }
+}
---
+++ EasyGoodLookUI/EasyGoodLookUI/Controls/TabItemEx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EasyGoodLookUI
+{
+    public class TabItemEx:TabItem
+    {
+        static TabItemEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TabItemEx), new FrameworkPropertyMetadata(typeof(TabItemEx)));
+        }
+    }
+}
