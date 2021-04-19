6f04739d33cdc51fe591e3efe6fa8c217394b446
jianghao <2564811788@163.com>
2021-04-19 15:06:34 +0800

    改变灯的实现

--- EasyGoodLookUI/EasyGoodLookUI/Controls/Light.cs
+++ EasyGoodLookUI/EasyGoodLookUI/Controls/Light.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -22,5 +23,32 @@ namespace EasyGoodLookUI
 
         public static readonly DependencyProperty IsRedProperty =
             DependencyProperty.Register("IsRed", typeof(bool), typeof(Light));
+        
+        public double LightWidth
+        {
+            get { return (double)GetValue(LightWidthProperty); }
+            set { SetValue(LightWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty LightWidthProperty =
+            DependencyProperty.Register("LightWidth", typeof(double), typeof(Light));
+
+        public double LightHeight
+        {
+            get { return (double)GetValue(LightHeightProperty); }
+            set { SetValue(LightHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty LightHeightProperty =
+            DependencyProperty.Register("LightHeight", typeof(double), typeof(Light));
+
+        public CornerRadius CRadius
+        {
+            get { return (CornerRadius)GetValue(CRadiusProperty); }
+            set { SetValue(CRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CRadiusProperty =
+            DependencyProperty.Register("CRadius", typeof(CornerRadius), typeof(Light));
     }
 }
