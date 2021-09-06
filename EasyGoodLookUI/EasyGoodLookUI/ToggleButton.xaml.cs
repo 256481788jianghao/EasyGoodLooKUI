87b2648716555580010ae3fd03417e8c6129d644
jianghao <2564811788@163.com>
2021-09-06 17:29:58 +0800

    add toggle button

---
+++ EasyGoodLookUI/EasyGoodLookUI/ToggleButton.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyGoodLookUI
+{
+    /// <summary>
+    /// ToggleButton.xaml 的交互逻辑
+    /// </summary>
+    public partial class ToggleButton : UserControl
+    {
+        public ToggleButton()
+        {
+            InitializeComponent();
+            R1.Width = xWidth;
+            R1.Height = xHeight;
+            Canvas.SetLeft(R2, 1);
+            Canvas.SetTop(R2, 1);
+            R2.Width = 0.5 * xWidth - 1;
+            R2.Height = xHeight - 2;
+            Canvas.SetLeft(R3, 1);
+            Canvas.SetTop(R3, 1);
+            R3.Width = 0.5 * xWidth - 1;
+            R3.Height = xHeight - 2;
+        }
+
+        public bool IsOn
+        {
+            get { return (bool)GetValue(IsOnProperty); }
+            set { SetValue(IsOnProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsOnProperty =
+            DependencyProperty.Register("IsOn", typeof(bool), typeof(ToggleButton), new PropertyMetadata(false));
+
+        public double xWidth
+        {
+            get { return (double)GetValue(xWidthProperty); }
+            set { SetValue(xWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty xWidthProperty =
+            DependencyProperty.Register("xWidth", typeof(double), typeof(ToggleButton), new PropertyMetadata(200.0));
+
+        public double xHeight
+        {
+            get { return (double)GetValue(xHeightProperty); }
+            set { SetValue(xHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty xHeightProperty =
+            DependencyProperty.Register("xHeight", typeof(double), typeof(ToggleButton), new PropertyMetadata(100.0));
+
+        double atime = 200;
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            IsOn = !IsOn;
+            if (IsOn)
+            {
+                Storyboard bd = new Storyboard();
+                DoubleAnimation da = new DoubleAnimation();
+                da.From = 1;
+                da.To = xWidth * 0.5;
+                da.Duration = new Duration(TimeSpan.FromMilliseconds(atime));
+                Storyboard.SetTarget(da,R3);
+                Storyboard.SetTargetProperty(da, new PropertyPath(Canvas.LeftProperty));
+                bd.Children.Add(da);
+                bd.Begin();
+                //Canvas.SetLeft(R3, xWidth * 0.5);
+            }
+            else
+            {
+                Storyboard bd = new Storyboard();
+                DoubleAnimation da = new DoubleAnimation();
+                da.From = xWidth * 0.5;
+                da.To = 1;
+                da.Duration = new Duration(TimeSpan.FromMilliseconds(atime));
+                Storyboard.SetTarget(da, R3);
+                Storyboard.SetTargetProperty(da, new PropertyPath(Canvas.LeftProperty));
+                bd.Children.Add(da);
+                bd.Begin();
+                //Canvas.SetLeft(R3, 1);
+            }
+        }
+    }
+}
