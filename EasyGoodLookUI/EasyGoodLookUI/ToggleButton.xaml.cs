511e84ee6e2f65c0049a3545be8980999583fc6d
jianghao <2564811788@163.com>
2021-09-07 08:43:34 +0800

    fix bug

--- EasyGoodLookUI/EasyGoodLookUI/ToggleButton.xaml.cs
+++ EasyGoodLookUI/EasyGoodLookUI/ToggleButton.xaml.cs
@@ -21,6 +21,9 @@ namespace EasyGoodLookUI
     /// </summary>
     public partial class ToggleButton : UserControl
     {
+
+        public delegate void ToggleButtonHandler(bool oldValue,bool newValue);
+        public event ToggleButtonHandler ToggleButtonEvent;
         public ToggleButton()
         {
             InitializeComponent();
@@ -34,7 +37,27 @@ namespace EasyGoodLookUI
             Canvas.SetTop(R3, 1);
             R3.Width = 0.5 * xWidth - 1;
             R3.Height = xHeight - 2;
+            Canvas.SetLeft(TextBlock_OFF, xWidth * 0.5 + 2);
+            Canvas.SetLeft(TextBlock_ON, 2);
+
+            if (IsOn)
+            {
+                Canvas.SetLeft(R3, xWidth * 0.5);
+            }
+            else
+            {
+                Canvas.SetLeft(R3, 1);
+            }
         }
+
+        public string NameStr
+        {
+            get { return (string)GetValue(NameStrProperty); }
+            set { SetValue(NameStrProperty, value); }
+        }
+
+        public static readonly DependencyProperty NameStrProperty =
+            DependencyProperty.Register("NameStr", typeof(string), typeof(ToggleButton), new PropertyMetadata("namestr"));
 
         public bool IsOn
         {
@@ -52,7 +75,7 @@ namespace EasyGoodLookUI
         }
 
         public static readonly DependencyProperty xWidthProperty =
-            DependencyProperty.Register("xWidth", typeof(double), typeof(ToggleButton), new PropertyMetadata(200.0));
+            DependencyProperty.Register("xWidth", typeof(double), typeof(ToggleButton), new PropertyMetadata(70.0));
 
         public double xHeight
         {
@@ -61,7 +84,7 @@ namespace EasyGoodLookUI
         }
 
         public static readonly DependencyProperty xHeightProperty =
-            DependencyProperty.Register("xHeight", typeof(double), typeof(ToggleButton), new PropertyMetadata(100.0));
+            DependencyProperty.Register("xHeight", typeof(double), typeof(ToggleButton), new PropertyMetadata(30.0));
 
         double atime = 200;
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
@@ -93,6 +116,7 @@ namespace EasyGoodLookUI
                 bd.Begin();
                 //Canvas.SetLeft(R3, 1);
             }
+            ToggleButtonEvent?.Invoke(!IsOn, IsOn);
         }
     }
 }
